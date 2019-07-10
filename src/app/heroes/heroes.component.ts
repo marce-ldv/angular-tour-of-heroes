20928365a18bd61a8809f9563965d4b7cb44f11d
MarceloLudovino <marcelo.docutec@gmail.com>
2019-07-10 17:30:33 -0300

    Add router and heroes details

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +10,25 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  private selectedHero: Hero =[];
+  private selectedHero: any = [];
+  // private heroes = HEROES;
+  private heroes: Hero[];
 
-  private heroes = HEROES;
-
-  constructor() {
+  constructor(private heroService: HeroService) {
   }
 
   public ngOnInit(): void {
-
+    this.getHeroes();
   }
 
   public onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    alert(this.selectedHero.name);
+    // alert(this.selectedHero.name);
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
 }
